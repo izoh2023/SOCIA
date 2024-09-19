bbacc0507a411ce9d1f047bc59155f88e288bfb6
isaac <isaacnjeru2002@gmail.com>
2024-09-20 02:22:05 +0300

    changes

--- app/lib/testingDb.tsx
+++ app/lib/testingDb.tsx
@@ -5,14 +5,14 @@ let pool: Pool | undefined;
 if (!pool) {
     pool = mysql.createPool({
         host: 'ci-cd.mysql.database.azure.com',
+        port:3306,
         user: 'isaac',
         password: 'Azure@success', // Ensure this is correct
         database: 'testing',
+        ssl: 'require',
         waitForConnections: true,
         connectionLimit: 10,
         queueLimit: 0,
         connectTimeout: 28800, //28 seconds
     });
 }
-
-export default pool!;
