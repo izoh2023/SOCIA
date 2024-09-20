0d791a852d6951299921075567ccf92019a99835
isaac <isaacnjeru2002@gmail.com>
2024-09-21 02:35:11 +0300

    change

--- app/restAPI/route.tsx
+++ app/restAPI/route.tsx
@@ -1,6 +1,5 @@
-
-import { collection } from "../lib/testingDb";
-// import { ResultSetHeader } from "mysql2";
+import pool from "@/app/lib/testingDb";
+import { ResultSetHeader } from "mysql2";
 import { NextRequest, NextResponse } from "next/server";
 
 interface myError {
@@ -13,65 +12,65 @@ interface myError {
 
 // API logic for all the CRUD operations
 const handler = async (req: NextRequest) => {
-    // let con;
+    let con;
     try {
-        // con = await collection.getConnection(); // Connect to the database
+        con = await pool.getConnection(); // Connect to the database
 
         switch (req.method) {
             case 'GET': {
-                const jsonObjects = collection.find()
-                return NextResponse.json(jsonObjects, { status: 200 });
+                const [rows] = await con.execute('SELECT ID, fname, sname, telNumber FROM person');
+                return NextResponse.json(rows, { status: 200 });
             }
 
-            // case 'POST': {
-            //     const body = await req.json();
-            //     const { ID, fname, sname, telNumber } = body;
-            //     if (!ID || !fname || !sname || !telNumber) {
-            //         return NextResponse.json('Missing required fields', { status: 404 });
-            //     }
-            //     const [results] = await con.execute<ResultSetHeader>(
-            //         'INSERT INTO person (ID, fname, sname, telNumber) VALUES (?, ?, ?, ?)',
-            //         [ID, fname, sname, telNumber]
-            //     );
-            //     if (results.affectedRows < 1) {
-            //         return NextResponse.json('Failed to create record', { status: 201 });
-            //     }
-            //     return NextResponse.json('Record created successfully', { status: 200 });
-            // }
+            case 'POST': {
+                const body = await req.json();
+                const { ID, fname, sname, telNumber } = body;
+                if (!ID || !fname || !sname || !telNumber) {
+                    return NextResponse.json('Missing required fields', { status: 404 });
+                }
+                const [results] = await con.execute<ResultSetHeader>(
+                    'INSERT INTO person (ID, fname, sname, telNumber) VALUES (?, ?, ?, ?)',
+                    [ID, fname, sname, telNumber]
+                );
+                if (results.affectedRows < 1) {
+                    return NextResponse.json('Failed to create record', { status: 201 });
+                }
+                return NextResponse.json('Record created successfully', { status: 200 });
+            }
 
-            // case 'PUT': {
-            //     const body = await req.json();
-            //     const { ID, fname, sname, telNumber } = body;
-            //     if (!ID || !fname || !sname || !telNumber) {
-            //         return NextResponse.json('Missing required fields', { status: 404 });
-            //     }
-            //     const [results] = await con.execute<ResultSetHeader>(
-            //         'UPDATE person SET fname = ?, sname = ?, telNumber = ? WHERE ID = ?',
-            //         [fname, sname, telNumber, ID]
-            //     );
-            //     if (results.affectedRows === 0) {
-            //         return NextResponse.json('ID does not exist', { status: 404 });
-            //     }
-            //     return NextResponse.json('Record updated successfully', { status: 200 });
-            // }
+            case 'PUT': {
+                const body = await req.json();
+                const { ID, fname, sname, telNumber } = body;
+                if (!ID || !fname || !sname || !telNumber) {
+                    return NextResponse.json('Missing required fields', { status: 404 });
+                }
+                const [results] = await con.execute<ResultSetHeader>(
+                    'UPDATE person SET fname = ?, sname = ?, telNumber = ? WHERE ID = ?',
+                    [fname, sname, telNumber, ID]
+                );
+                if (results.affectedRows === 0) {
+                    return NextResponse.json('ID does not exist', { status: 404 });
+                }
+                return NextResponse.json('Record updated successfully', { status: 200 });
+            }
 
-            // case 'DELETE': {
-            //     const body = await req.json();
-            //     const { ID } = body;
-            //     if (!ID) {
-            //         return NextResponse.json('Missing ID field', { status: 404 });
-            //     }
-            //     const [results] = await con.execute<ResultSetHeader>(
-            //         'DELETE FROM person WHERE ID = ?',
-            //         [ID]
-            //     );
-            //     if (results.affectedRows === 0) {
-            //         return NextResponse.json(
-            //             'ID does not exist',
-            //             { status: 404 });
-            //     }
-            //     return NextResponse.json('Record deleted successfully', { status: 200 });
-            // }
+            case 'DELETE': {
+                const body = await req.json();
+                const { ID } = body;
+                if (!ID) {
+                    return NextResponse.json('Missing ID field', { status: 404 });
+                }
+                const [results] = await con.execute<ResultSetHeader>(
+                    'DELETE FROM person WHERE ID = ?',
+                    [ID]
+                );
+                if (results.affectedRows === 0) {
+                    return NextResponse.json(
+                        'ID does not exist',
+                        { status: 404 });
+                }
+                return NextResponse.json('Record deleted successfully', { status: 200 });
+            }
 
             default:
                 return NextResponse.json('Method not allowed', { status: 405 });
@@ -83,26 +82,25 @@ const handler = async (req: NextRequest) => {
             return NextResponse.json('This ID already exists in the system', { status: 409 });
         }
         return NextResponse.json('Failed to connect to the database', { status: 500 });
-    } 
-    // finally {
-    //     if (con) {
-    //         con.release(); // Ensure the connection is released
-    //     }
-    // }
+    } finally {
+        if (con) {
+            con.release(); // Ensure the connection is released
+        }
+    }
 };
 
 export async function GET(req: NextRequest) {
     return handler(req);
 }
 
-// export async function POST(req: NextRequest) {
-//     return handler(req);
-// }
+export async function POST(req: NextRequest) {
+    return handler(req);
+}
 
-// export async function PUT(req: NextRequest) {
-//     return handler(req);
-// }
+export async function PUT(req: NextRequest) {
+    return handler(req);
+}
 
-// export async function DELETE(req: NextRequest) {
-//     return handler(req);
-// }
+export async function DELETE(req: NextRequest) {
+    return handler(req);
+}
